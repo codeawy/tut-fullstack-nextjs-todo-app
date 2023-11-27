56b631c432ccb583896539f0d35707e453ed2685
codeawy <codeawy@gmail.com>
2023-11-27 10:53:43 +0200

    Document server actions and update markdown file

--- README.md
+++ README.md
@@ -1,36 +1,41 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Full Stack Todo App with Next.js, Prisma, and MongoDB
+
+This is a full-stack todo app built with Next.js, TypeScript, Prisma, and MongoDB. It incorporates various features and technologies to provide a comprehensive example of a modern web application.
+
+## Features
+
+- **Next.js & TypeScript:** Utilizes the power of Next.js for server-side rendering and TypeScript for type safety.
+- **Prisma & MongoDB:** Implements Prisma as the database toolkit and MongoDB for data storage.
+- **Server Actions:** Implements server-side actions for enhanced functionality.
+- **Clerk for Authentication:** Utilizes Clerk for secure and easy authentication.
+- **Add Todo to Logged In User:** Ensures that adding todos is restricted to logged-in users only.
+- **Database Seeding:** Includes a database seeding mechanism for initial data population.
+- **Shadcn UI:** Uses Shadcn UI for pre-built components, enhancing the app's visual appeal.
+- **Toggle Dark Mode:** Incorporates a dark mode feature for user customization.
+- **CRUD Operations:** Provides basic CRUD (Create, Read, Update, Delete) operations for managing todos.
 
 ## Getting Started
 
-First, run the development server:
+### Prerequisites
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+- Node.js and npm installed
+- MongoDB server running
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+### Installation
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+1. Clone the repository:
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+   ```bash
+   git clone https://github.com/codeawy/tut-fullstack-nextjs-todo-app.git
+   ```
 
-## Learn More
+2. Install dependencies:
+   `   npm install`
+   3.Set up environment variables:
 
-To learn more about Next.js, take a look at the following resources:
+   Create a `.env` file in the root directory with the necessary environment variables. Include details such as `MongoDB` connection string, `Clerk API key`, etc.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+3. Run the development server:
+   `    npm run dev
+   `
+   Open http://localhost:3000 in your browser to view the app.
--- actions/todo.actions.ts
+++ actions/todo.actions.ts
@@ -6,7 +6,15 @@ import { revalidatePath } from "next/cache";
 
 const prisma = new PrismaClient();
 
-export const getUserTodoListAction = async ({ userId }: { userId: string | null }) => {
+/**
+ * Retrieves the todo list for a specific user.
+ *
+ * @param {Object} params - The parameters object.
+ * @param {string | null} params.userId - The ID of the user whose todo list is to be retrieved.
+ * @returns {Promise<Array<ITodo>>} - A promise that resolves to an array of todo items.
+ * @throws {Error} - Throws an error if something goes wrong during the retrieval process.
+ */
+export const getUserTodoListAction = async ({ userId }: { userId: string | null }): Promise<Array<ITodo>> => {
   try {
     return await prisma.todo.findMany({
       where: {
@@ -21,6 +29,17 @@ export const getUserTodoListAction = async ({ userId }: { userId: string | null 
   }
 };
 
+/**
+ * Creates a new todo item.
+ *
+ * @param {Object} params - The parameters object.
+ * @param {string} params.title - The title of the todo item.
+ * @param {string | undefined} params.body - The body or description of the todo item.
+ * @param {boolean} params.completed - Indicates whether the todo item is completed or not.
+ * @param {string | null} params.userId - The ID of the user creating the todo item.
+ * @returns {Promise<void>} - A promise that resolves when the todo item is successfully created.
+ * @throws {Error} - Throws an error if something goes wrong during the creation process.
+ */
 export const createTodoAction = async ({
   title,
   body,
@@ -31,7 +50,7 @@ export const createTodoAction = async ({
   body?: string | undefined;
   completed: boolean;
   userId: string | null;
-}) => {
+}): Promise<void> => {
   try {
     await prisma.todo.create({
       data: {
@@ -47,7 +66,15 @@ export const createTodoAction = async ({
     throw new Error("Something went wrong");
   }
 };
-export const deleteTodoAction = async ({ id }: { id: string }) => {
+
+/**
+ * Deletes a todo item.
+ *
+ * @param {Object} params - The parameters object.
+ * @param {string} params.id - The ID of the todo item to be deleted.
+ * @returns {Promise<void>} - A promise that resolves when the todo item is successfully deleted.
+ */
+export const deleteTodoAction = async ({ id }: { id: string }): Promise<void> => {
   await prisma.todo.delete({
     where: {
       id,
@@ -56,7 +83,19 @@ export const deleteTodoAction = async ({ id }: { id: string }) => {
 
   revalidatePath("/");
 };
-export const updateTodoAction = async ({ id, title, body, completed }: ITodo) => {
+
+/**
+ * Updates a todo item.
+ *
+ * @param {ITodo} params - The parameters object containing the todo item details.
+ * @param {string} params.id - The ID of the todo item to be updated.
+ * @param {string} params.title - The updated title of the todo item.
+ * @param {string | undefined} params.body - The updated body or description of the todo item.
+ * @param {boolean} params.completed - Indicates whether the todo item is completed or not.
+ * @returns {Promise<void>} - A promise that resolves when the todo item is successfully updated.
+ * @throws {Error} - Throws an error if something goes wrong during the update process.
+ */
+export const updateTodoAction = async ({ id, title, body, completed }: ITodo): Promise<void> => {
   try {
     await prisma.todo.update({
       where: {
